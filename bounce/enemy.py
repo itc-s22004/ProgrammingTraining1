1a10df30ca39ab3ee0030981175232351e442a6e
itc-s22004 <s22004@std.it-college.ac.jp>
2022-08-26 09:47:38 +0000

    first

--- bounce/enemy.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from PIL import Image, ImageTk
-
-class Enemy:
-    def __init__(self, ball):
-        self.ball = ball
-        app = tk.Tk()
-        img = Image.open('enemy.jpg')
